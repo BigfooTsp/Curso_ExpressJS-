96db78282f1a1d8269984f3d3b1e17b762e08ef9
Pedro Pina <pedrojosepinacuenca@gmail.com>
2019-06-02 20:18:14 +0200

    v.1.0.1 Añadida gestión de vistas en test

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Index');
+    res.render('index');
 });
 
 router.get('/about', (req, res) => {
-    res.send('About');
+    res.render('about');
 });
 
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/users/signin', (req, res) => {
-    res.send('Ingresando en la app');
+    res.render('users/signin');
 });
 
 router.get('/users/signup', (req, res) => {
-    res.send('Formulario de autentificación');
+    res.render('users/signup');
 });
 
 
