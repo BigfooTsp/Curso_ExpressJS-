2b7883b1d6e05704ac8691e1884472aa50e5e78e
Pedro Pina <pedrojosepinacuenca@gmail.com>
2019-06-07 09:23:23 +0200

    ... apunte. Estructura básica de controladores

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -7,5 +7,25 @@
 
 const ctrl = {};
 
+ctrl.index = (req, res) => {
+  
+};
+
+ctrl.create = (req, res) => {
+
+};
+
+
+ctrl.like = (req, res) => {
+
+};
+
+ctrl.comment = (req, res) => {
+
+};
+
+ctrl.remove = (req, res) => {
+
+};
 
 module.exports = ctrl;
